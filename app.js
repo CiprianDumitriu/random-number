08b6568ab113818d18a020f849833448c3d0a862
CiprianDumitriu <115021619+CiprianDumitriu@users.noreply.github.com>
2022-11-23 15:26:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-const button1 = document.querySelector("#button1")
-const button3 = document.querySelector("#button3")
-const button2 = document.querySelector("#button2")
+ const button1 = document.querySelector("#button1")
+ const button3 = document.querySelector("#button3")
+ const button2 = document.querySelector("#button2")
+ const body = document.querySelector("body")
+
+//  Prima rezolvare
+// button1.addEventListener("click", () => {
+//     chooseWinner(0);
+// })
+
+// button2.addEventListener("click", () => {
+//     chooseWinner(1);
+// })
+
+// button3.addEventListener("click", () => {
+//     chooseWinner(2);
+// })
 
 
-button1.addEventListener("click", () => {
-    chooseWinner(0);
+
+body.addEventListener('click', e => {
+    if (e.target == button1) {
+        chooseWinner(0);
+    } else if (e.target == button2) {
+        chooseWinner(1);
+    } else if (e.target == button3) {
+        chooseWinner(2);
+    } else {
+        return
+    }
 })
 
-button2.addEventListener("click", () => {
-    chooseWinner(1);
-})
-
-button3.addEventListener("click", () => {
-    chooseWinner(2);
-})
 
 function chooseWinner(clickedButton) {
     let winner = Math.floor(Math.random() * 3)
